ad617c456928920f6225e81cb43f414f0dab8b5c
Rich Seviora <richard.seviora@gmail.com>
2014-11-04 13:55:51 -0500

    Created Photos Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'photos#index'
+  resources :photo
+  # This will invoke the welcome controller, and activate the index action.
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
