7c982d9e6a2f3b8a222199070cfe1449a4c4c8e0
Rich Seviora <richard.seviora@gmail.com>
2014-11-05 14:06:21 -0500

    Modular Link Rendering, Font Awesome, Headers
    Also deleted unnecessary views.

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,10 @@
 module PhotosHelper
+  def list_group_link(path,text,classes)
+    if current_page?(path)
+      # Necessary to state partial, otherwise locals are not passed to partial.
+      render partial: 'list_item_current', locals: {path: path, text: text, classes: classes}
+    else
+      render partial: 'list_item_standard', locals: {path: path, text: text, classes: classes}
+    end
+  end
 end
